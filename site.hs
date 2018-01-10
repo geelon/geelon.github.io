c33e58cf6b4589acc59067212f76fdcae2170f19
Geelon So <geelon@uchicago.edu>
2018-01-10 15:40:20 -0500

    Wed Jan 10 15:40:11 EST 2018

--- site.hs
+++ site.hs
@@ -5,6 +5,9 @@ import           Hakyll
 import qualified Data.Set as S
 import           Data.Maybe
 import           Text.Pandoc.Options
+import qualified Text.HTML.TagSoup as T
+import           Data.List.Utils
+import           Control.Monad.Loops (concatM)
 
 --------------------------------------------------------------------------------
 main :: IO ()
@@ -34,10 +37,20 @@ main = hakyll $ do
       >>= loadAndApplyTemplate "templates/layer1.html" postCtx
       >>= relativizeUrls
 
-  match "projects/*" $ do
-    route $ setExtension "html"
+
+  match "projects/notes/readme*" $ do
+    route $ constRoute "projects/notes/index.html"
     compile $ pandocMathCompiler
       >>= loadAndApplyTemplate "templates/layer1.html" postCtx
+      >>= mdToHTML
+      >>= relativizeUrls
+
+  match ("projects/notes/**" .&&. complement "projects/notes/readme*") $ do
+    route $ setExtension "html"
+    compile $ pandocMathCompiler
+      >>= loadAndApplyTemplate "templates/writing-body.html"    postCtx
+      >>= loadAndApplyTemplate "templates/layer1.html" postCtx
+      >>= liftURLlevel 2
       >>= relativizeUrls
 
   match ("me/*" .||. "projects/*") $ do
@@ -63,6 +76,7 @@ main = hakyll $ do
         >>= loadAndApplyTemplate "templates/archive.html" archiveCtx
         >>= loadAndApplyTemplate "templates/layer0.html" archiveCtx
         >>= relativizeUrls
+        >>= mdToHTML
 
 
   match "index.html" $ do
@@ -89,3 +103,41 @@ pandocMathCompiler =
                           writerHTMLMathMethod = MathJax ""
                         }
     in pandocCompilerWith defaultHakyllReaderOptions writerOptions
+
+
+
+mdToHTML :: Item String -> Compiler (Item String)
+mdToHTML item = do
+    route <- getRoute $ itemIdentifier item
+    return $ case route of
+        Nothing -> item
+        Just r  -> fmap mdToHTMLWith item
+
+
+-- | Relativize URL's in HTML
+mdToHTMLWith :: String  -- ^ HTML to relativize
+             -> String  -- ^ Resulting HTML
+mdToHTMLWith = withUrls md
+  where
+    md x
+      | endswith ".md" x = (replace ".md" ".html" <$> replace "./" "./notes/") x
+      | otherwise = x
+
+
+liftURL :: Item String -> Compiler (Item String)
+liftURL item = do
+    route <- getRoute $ itemIdentifier item
+    return $ case route of
+        Nothing -> item
+        Just r  -> fmap (withUrls up) item
+          where
+            up x
+              | startswith "http" x = x
+              | startswith "." x = "../" ++ x
+              | otherwise = x
+
+liftURLlevel :: Int -> Item String -> Compiler (Item String)
+liftURLlevel n = concatM $ replicate n liftURL
+--------------------------------------------------------------------------------
+
+
